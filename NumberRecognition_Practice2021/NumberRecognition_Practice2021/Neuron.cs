1de282f1ef2c765f8c63063a4903c7c35324327c
Lichuhah <belovanton.work@gmail.com>
2021-07-01 14:41:31 +0300

    add class Neuron

---
+++ NumberRecognition_Practice2021/NumberRecognition_Practice2021/Neuron.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberRecognition_Practice2021
+{
+    class Neuron
+    {
+        public double[] weights; //веса 
+        public double lastActivation; //последняя активация
+        public double bias; //сдвиг
+        public static double Sigmoid(double input)//сигмоида
+        {
+            return 1 / (1 + Math.Exp(-input));
+        }
+        public static double SigmoidDerivated(double input)//производная от сигмоиды
+        {
+            double y = Sigmoid(input);
+            return y * (1 - y);
+        }
+
+        public Neuron(int numberOfInputs, Random r) //конструктор (кол-во входов, рандом)
+        {
+            bias = 10 * r.NextDouble() - 5;
+            weights = new double[numberOfInputs];
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                weights[i] = 10 * r.NextDouble() - 5;
+            }
+        }
+        public double Activate(double[] inputs) //активация массив входов
+        {
+            double activation = bias;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                activation += weights[i] * inputs[i];
+            }
+
+            lastActivation = activation;
+            return Sigmoid(activation);
+        }
+    }
+}
